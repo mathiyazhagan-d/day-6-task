23885b9748fe259b0ccc8bab55f5144eb7207e7f
mathiyazhagan-d <116990455+mathiyazhagan-d@users.noreply.github.com>
2023-01-13 14:34:32 +0530

    fis

--- 4-Uber price.js
+++ 4-Uber price.js
@@ -1,33 +1,26 @@
-
-console.log("")//for allignment perposer
+console.log(""); //for allignment perposer
 console.log("4-Uber price");
-console.log("")
-
-
+console.log("");
 
 class Uber {
-    constructor(distance,WaitTimeCar,travelTimeInHour) {
-        this.distance = distance;
-        this.WaitTimeCar=WaitTimeCar;
-      this.time=travelTimeInHour;
+  constructor(distance, WaitTimeCar, travelTimeInHour) {
+    this.distance = distance;
+    this.WaitTimeCar = WaitTimeCar;
+    this.time = travelTimeInHour;
+  }
+
+  price() {
+    let amount;
+    if (this.distance > 0 && this.distance < 30) {
+      this.rate = 8;
+    } else {
+      this.rate = 5;
     }
-
-        price()
-        {
-            let amount;
-            if (this.distance > 0 && this.distance < 30) {
-                this.rate = 8
-            }
-            else {
-                this.rate = 5
-            }
-            return `The customer Journey Time ${this.time} hour.
+    return `The customer Journey Time ${this.time} hour.
 waiting time for uber is ${this.WaitTimeCar} min
-and the price = ${this.rate*this.distance-(3*this.WaitTimeCar)}/-`;
-        }
-    
-
+and the price = ${this.rate * this.distance - 3 * this.WaitTimeCar}/-`;
+  }
 }
-let customer = new Uber(35,10,1)
+let customer = new Uber(35, 10, 1);
 
 console.log(customer.price());
--- readme.md
+++ readme.md
@@ -4,9 +4,11 @@
   
 ## All js file are linked in index.html
 
-# QUESTION NO 1 => 1-Class movie.js
-# QUESTION NO 2 => 2-Circle class.js
-# QUESTION NO 3 => 3-person details.js
-# QUESTION NO 4 => 4-Uber price.js
+## QUESTION NO 1 => 1-Class movie.js
+## QUESTION NO 2 => 2-Circle class.js
+## QUESTION NO 3 => 3-person details.js
+## QUESTION NO 4 => 4-Uber price.js
 
+## you can see the output console in the netlify link given below
 
+# NETLIFY LINK => <span><a href="https://celadon-marzipan-92b6c0.netlify.app/" target="_blank">click me</a></span>
